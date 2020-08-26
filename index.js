cc0769bb73fd600784aa247ed5b77698cff002a6
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-26 22:31:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => console.log(json));
 
 }
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
